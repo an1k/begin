a245d4f7aaf205431893182c6e426d80b2bcb9de
Anik Bose <a-anikb@SINGTELCDC.COM>
2019-12-06 17:28:05 +0800

    changed the backend url

--- helpers/backendBridge.js
+++ helpers/backendBridge.js
@@ -1,5 +1,5 @@
 axios=require('axios')
-url=process.env.BCKURL || 'http://localhost:8082/'
+url=process.env.BCKURL || 'http://begin-backend-git-testproject.apps.openshift-test.accenturemywizard-aaam.com.au/test/'
 
 module.exports.getBayDetailsByID=function(id){
     return axios.post(url+"getBayDetails",{
