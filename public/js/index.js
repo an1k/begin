0ffdb61767bf244a65b591abd14afc533875844a
Anik Bose <a-anikb@SINGTELCDC.COM>
2019-12-06 18:07:05 +0800

    added fading

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,13 @@ function checkBay(v){
     function(data, status){
         if(status=='success'){
             console.log(data)
+            $("#bay_info").fadeIn(3000);
             $('#display').html(data.name)
             $('#displayResourceNum').html(data.resource_number)
             $('#displayOrganisation').html(data.organisation)
         }else{
             $('#display').html("There was some error fetching the data")
+            $("#bay_info").fadeOut(3000);
         }
       
     });
